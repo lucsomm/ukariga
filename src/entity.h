2eab3ae89d07c91ce4a1c16b8c2471473541d371
potatislc <lucalbin@gmail.com>
2026-05-26 21:24:55 +0200

    Extract functions from entity.h into math_utils.h

--- src/entity.h
+++ src/entity.h
@@ -5,14 +5,6 @@
 #include "glm/vec2.hpp"
 
 namespace ukariga {
-    inline Vector2 to_ray_vec(const glm::vec2& vec) {
-        return Vector2{vec.x, vec.y};
-    }
-
-    inline glm::vec2 to_glm_vec(const Vector2& vec) {
-        return glm::vec2{vec.x, vec.y};
-    }
-
     class EntityBase {
     public:
         using TypeId = uint16_t;
